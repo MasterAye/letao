9020820fbdfa9eed2d7ef23adc7fdf8557c3edf4
zhu <zz170721@163.com>
2017-12-26 22:26:40 +0800

    2017-12-26---22:26提交

---
+++ public/m/js/index.js
@@ -0,0 +1,19 @@
+
+
+
+window.addEventListener('load',function(){
+    mui('.mui-slider').slider({
+        interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.006,//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        scrollY: true, //是否竖向滚动
+        scrollX: false, //是否横向滚动
+        startX: 0, //初始化时滚动至x
+        startY: 0, //初始化时滚动至y
+        indicators: true, //是否显示滚动条
+        bounce: true //是否启用回弹
+    });
+
+});
